82e0b27b47c3dd839203bcfb5ab6425050d69bac
deepakJNC <deepak.mern@jnctechlabs.com>
2023-01-04 17:57:15 +0530

    table with accounte detail

---
+++ build/precache-manifest.1695f69e3b0c47b32d7d2c299c0e7f99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ad843836df0d7b7eb05",
+    "url": "/static/css/main.7eeeff1b.chunk.css"
+  },
+  {
+    "revision": "6ad843836df0d7b7eb05",
+    "url": "/static/js/main.6ad84383.chunk.js"
+  },
+  {
+    "revision": "69e0dab03ec9f6c99eb0",
+    "url": "/static/js/1.69e0dab0.chunk.js"
+  },
+  {
+    "revision": "14f65cd06a3475642121",
+    "url": "/static/js/2.14f65cd0.chunk.js"
+  },
+  {
+    "revision": "56939e6b754efe784d27",
+    "url": "/static/js/runtime~main.56939e6b.js"
+  },
+  {
+    "revision": "ea6007c96fe98ea7888fe5c61451c02a",
+    "url": "/index.html"
+  }
+];
